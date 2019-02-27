36fd943cb570679f04968fbb225884c356b4b4c1
melvinanthonyrancap <mel.rancap@gmail.com>
2019-02-26 19:40:50 -0800

    Added Problem description

--- Answers/exercise1.py
+++ Answers/exercise1.py
@@ -3,3 +3,16 @@ os.system('cls')
 
 name = input("What is your name? ")
 print(f"Hello, {name}, nice to meet you!")
+
+"""
+Saying Hello
+The “Hello, World” program is the first program you learn
+to write in many languages, but it doesn’t involve any input.
+So create a program that prompts for your name and prints
+a greeting using your name.
+Example Output
+What is your name? Brian
+Hello, Brian, nice to meet you!
+Constraint
+• Keep the input, string concatenation, and output separate.
+"""
